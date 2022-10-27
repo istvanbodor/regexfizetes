4797159989ec880ba925e4492345591899f2f1ff
Bodor István <istvanbodor19@gmail.com>
2022-10-27 12:12:03 +0200

    pontositas

--- src/index.js
+++ src/index.js
@@ -3,48 +3,51 @@ document.addEventListener("DOMContentLoaded", () => {
     var _a;
     (_a = document.getElementById("fizetes")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b, _c, _d, _e, _f, _g, _h;
-        let nevellenorzo = /[a-z]{1,}/;
+        let nevellenorzo = /^[a-z]{1,}$/;
         let nev = (_a = document.getElementById('nev')) === null || _a === void 0 ? void 0 : _a.value;
-        let orszagellenorzo = /[a-z]{1,}/;
+        let orszagellenorzo = /^[a-z]{1,}$/;
         let orszag = (_b = document.getElementById('orszag')) === null || _b === void 0 ? void 0 : _b.value;
-        let varosellenorzo = /[a-z]{1,}/;
+        let varosellenorzo = /^[a-z]{1,}$/;
         let varos = (_c = document.getElementById('varos')) === null || _c === void 0 ? void 0 : _c.value;
-        let utcaellenorzo = /[a-z 0-9]{1,}/;
+        let utcaellenorzo = /^[a-z 0-9]{1,}$/;
         let utca = (_d = document.getElementById('utca')) === null || _d === void 0 ? void 0 : _d.value;
-        let iranyitoszamellenorzo = /[A-Z0-9]{1,}/;
+        let iranyitoszamellenorzo = /^[A-Z0-9]{1,}$/;
         let iranyitoszam = (_e = document.getElementById('iranyitoszam')) === null || _e === void 0 ? void 0 : _e.value;
-        let kartyaellenorzo = /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/;
+        let kartyaellenorzo = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/;
         let kartya = (_f = document.getElementById('kartya')) === null || _f === void 0 ? void 0 : _f.value;
-        let cvcellenorzo = /[0-9]{3}/;
+        let cvcellenorzo = /^[0-9]{3}$/;
         let cvc = (_g = document.getElementById('cvc')) === null || _g === void 0 ? void 0 : _g.value;
-        let kartyanevellenorzo = /[a-zA-Z" "]/;
+        let kartyanevellenorzo = /^[a-zA-Z" "]$/;
         let kartyanev = (_h = document.getElementById('kartyanev')) === null || _h === void 0 ? void 0 : _h.value;
         if (!nevellenorzo.test(nev)) {
-            alert("A név mező helytelen");
+            document.getElementById('hiba').textContent = "A név mező helytelen";
         }
         else if (!orszagellenorzo.test(orszag)) {
-            alert("Az ország mező helytelen");
+            document.getElementById('hiba').textContent = "Az ország mező helytelen";
         }
         else if (!varosellenorzo.test(varos)) {
-            alert("A város mező helytelen");
+            document.getElementById('hiba').textContent = "A város mező helytelen";
         }
         else if (!utcaellenorzo.test(utca)) {
-            alert("Az utca mező helytelen");
+            document.getElementById('hiba').textContent = "Az utca mező helytelen";
         }
         else if (!iranyitoszamellenorzo.test(iranyitoszam)) {
-            alert("Az irányítószám mező helytelen");
+            document.getElementById('hiba').textContent = "Az irányítószám mező helytelen";
         }
         else if (!kartyaellenorzo.test(kartya)) {
-            alert("A kártya mező helytelen");
+            document.getElementById('hiba').textContent = "A bankkártya mező helytelen";
         }
         else if (!cvcellenorzo.test(cvc)) {
-            alert("A cvc mező helytelen");
+            document.getElementById('hiba').textContent = "A cvc mező helytelen";
         }
         else if (!kartyanevellenorzo.test(kartyanev)) {
-            alert("A kártyára írt név mező helytelen");
+            document.getElementById('hiba').textContent = "A kártyára írt név mező helytelen";
         }
         else {
             document.body.innerHTML = "Siker";
+            document.body.style.color = "green";
+            document.body.style.fontSize = "50px";
+            document.body.style.marginTop = "200px";
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,73 +3,69 @@ document.addEventListener("DOMContentLoaded", ()=> {
         document.getElementById("fizetes")?.addEventListener("click", ()=>{
 
 
-                let nevellenorzo = /[a-z]{1,}/
+                let nevellenorzo = /^[a-z]{1,}$/
                 let nev : string= (document.getElementById('nev') as HTMLInputElement)?.value;
 
-                let orszagellenorzo = /[a-z]{1,}/
+                let orszagellenorzo = /^[a-z]{1,}$/
                 let orszag : string= (document.getElementById('orszag') as HTMLInputElement)?.value;
                 
-                let varosellenorzo = /[a-z]{1,}/
+                let varosellenorzo = /^[a-z]{1,}$/
                 let varos : string= (document.getElementById('varos') as HTMLInputElement)?.value;
 
-                let utcaellenorzo = /[a-z 0-9]{1,}/
+                let utcaellenorzo = /^[a-z 0-9]{1,}$/
                 let utca : string= (document.getElementById('utca') as HTMLInputElement)?.value;
                 
-                let iranyitoszamellenorzo = /[A-Z0-9]{1,}/
+                let iranyitoszamellenorzo = /^[A-Z0-9]{1,}$/
                 let iranyitoszam : string= (document.getElementById('iranyitoszam') as HTMLInputElement)?.value;
                 
-                let kartyaellenorzo = /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/
+                let kartyaellenorzo = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/
                 let kartya : string= (document.getElementById('kartya') as HTMLInputElement)?.value;
                 
-                let cvcellenorzo = /[0-9]{3}/
+                let cvcellenorzo = /^[0-9]{3}$/
                 let cvc : string= (document.getElementById('cvc') as HTMLInputElement)?.value;
 
-                let kartyanevellenorzo = /[a-zA-Z" "]/
+                let kartyanevellenorzo = /^[a-zA-Z" "]$/
                 let kartyanev : string= (document.getElementById('kartyanev') as HTMLInputElement)?.value;
                 
                 if (!nevellenorzo.test(nev)){
-                    alert("A név mező helytelen");
+                   (document.getElementById('hiba') as HTMLParagraphElement).textContent = "A név mező helytelen"
                 }
                 else if (!orszagellenorzo.test(orszag))
                 {
-                    alert("Az ország mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "Az ország mező helytelen"
+                    
                 }
                 else if (!varosellenorzo.test(varos))
                 {
-                    alert("A város mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "A város mező helytelen"
                 }
                 else if (!utcaellenorzo.test(utca))
                 {
-                    alert("Az utca mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "Az utca mező helytelen"
                 }   
                 else if (!iranyitoszamellenorzo.test(iranyitoszam))
                 {
-                    alert("Az irányítószám mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "Az irányítószám mező helytelen"
                 }
                 else if (!kartyaellenorzo.test(kartya))
                 {
-                    alert("A kártya mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "A bankkártya mező helytelen"
                 }
                 else if (!cvcellenorzo.test(cvc))
                 {
-                    alert("A cvc mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "A cvc mező helytelen"
                 }
                 else if (!kartyanevellenorzo.test(kartyanev))
                 {
-                    alert("A kártyára írt név mező helytelen");
+                    (document.getElementById('hiba') as HTMLParagraphElement).textContent = "A kártyára írt név mező helytelen"
                 }
                 else {
                     document.body.innerHTML ="Siker";
+                    document.body.style.color = "green";
+                    document.body.style.fontSize = "50px";
+                    document.body.style.marginTop = "200px";
 
                 }
-
-
-
-
-
-
-
-
 
 
 
